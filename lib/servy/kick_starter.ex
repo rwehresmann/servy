50def684c51f595033ef7fa0b99c49bff7be36c5
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2021-03-17 14:21:31 -0300

    Create a GenServer to recovery from HttpServer in case of unexpected exits

---
+++ lib/servy/kick_starter.ex
@@ -0,0 +1,40 @@
+defmodule Servy.KickStarter do
+  use GenServer
+
+  def start do
+    IO.puts "Starting the kickstarter..."
+    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def get_server do
+    GenServer.call __MODULE__, :get_server
+  end
+
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    server_pid = start_server()
+
+    {:ok, server_pid}
+  end
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_info({:EXIT, _pid, reason}, state) do
+    IO.puts "HttpServer exited (#{inspect reason})"
+
+    server_pid = start_server()
+
+    {:noreply, server_pid}
+  end
+
+  defp start_server do
+    IO.puts "Starting the HTTP server..."
+    
+    server_pid = spawn_link(Servy.HttpServer, :start, [4000])    
+    Process.register(server_pid, :http_server)
+
+    server_pid
+  end
+end
